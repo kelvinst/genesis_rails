7a05277cfadfe2739d6ab305aaf850edf29a7de3
kelvinst <kelvin.stinghen@gmail.com>
2016-04-07 21:04:18 -0300

    Fixing wrong root var reference

--- lib/generators/genesis_rails/install/install_generator.rb
+++ lib/generators/genesis_rails/install/install_generator.rb
@@ -8,12 +8,12 @@ module GenesisRails
     def install_files
       templates = self.class.source_root.join "templates"
       Dir[templates.join "**/*.*"].each do |file|
-        copy_file file, "lib/templates/#{file.gsub(root.to_s, "")}"
+        copy_file file, "lib/templates/#{file.gsub(templates.to_s, "")}"
       end
 
       spec_support = self.class.source_root.join "spec/support"
       Dir[spec_support.join "**/*.*"].each do |file|
-        copy_file file, "spec/support/#{file.gsub(root.to_s, "")}"
+        copy_file file, "spec/support/#{file.gsub(spec_support.to_s, "")}"
       end
     end
   end
